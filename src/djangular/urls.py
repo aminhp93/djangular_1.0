0c312033dcc245243e55652a6b30e135587a926e
aminhp93 <minhpn.org.ec@gmail.com>
2017-04-06 08:40:14 -0700

    06 - Single Page App Template View

--- src/djangular/urls.py
+++ src/djangular/urls.py
@@ -17,24 +17,25 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 from rest_framework_jwt.views import obtain_jwt_token
 
-from accounts.views import (login_view, register_view, logout_view)
+# from accounts.views import (login_view, register_view, logout_view)
 # from posts import views as posts_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # url(r'^posts/', posts_views.post_home),
-    url(r'^login/', login_view, name="login"),
-    url(r'^logout/', logout_view, name="logout"),
-    url(r'^register/', register_view, name="register"),
-    url(r'^', include("posts.urls", namespace="posts")),
+    # url(r'^login/', login_view, name="login"),
+    # url(r'^logout/', logout_view, name="logout"),
+    # url(r'^register/', register_view, name="register"),
+    # url(r'^', include("posts.urls", namespace="posts")),
     url(r'^api/auth/token/', obtain_jwt_token),
     url(r'^api/posts/', include("posts.api.urls", namespace="posts-api")),
     url(r'^api/users/', include("accounts.api.urls", namespace="users-api")),
     url(r'^api/comments/', include("comments.api.urls", namespace="comments-api")),
-    url(r'^comments/', include("comments.urls", namespace="comments")),
+    # url(r'^comments/', include("comments.urls", namespace="comments")),
 
     
 ]
@@ -42,3 +43,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    url(r'', TemplateView.as_view(template_name="ang/home.html")),
+]
